2aeb169b71a0b9df5265a22b06522416d4b2e420
Ngo Gia Bao <tranbaoelie@gmail.com>
2026-01-26 21:30:40 -0500

    add more comments

---
+++ vector/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/LEETCODE/LEARN/cpp_ds/vector
+# Build directory: D:/LEETCODE/LEARN/cpp_ds/vector/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("D:/LEETCODE/LEARN/cpp_ds/vector/build/my_vector_tests[1]_include.cmake")
+subdirs("_deps/googletest-build")
