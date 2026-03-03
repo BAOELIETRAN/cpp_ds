845a0d1902d04c4d70afa90757151608dd905ccd
BAOELIETRAN <bngo@oberlin.edu>
2026-03-03 10:53:39 -0500

    hash map - first few steps

--- hash_map/src/hash_map.h
+++ hash_map/src/hash_map.h
@@ -11,18 +11,36 @@
 // initialization order: base class --> data member (in declaring order) --> constructor body
 namespace anh_phan{
     class hash_map{
-        // load factor = # of stored items / # of available buckets
-        // bucket == doubly linked list --> std::list
         private:
             static constexpr std::size_t default_capacity = 11;
             static constexpr float default_load_factor = 0.75f;
+            // bucket == doubly linked list --> std::list
+            // linked list of <key, value>
             std::array<std::list<std::pair<int, int>>, default_capacity>* hash_table{};
             // number of buckets -- size of hash table
-            std::size_t m_bucket_count{default_capacity};
+            std::size_t m_capacity{};
+            // load factor = # of stored items / # of available buckets
+            float m_load_factor{};
             // max number of elements
-            std::size_t m_threshold{static_cast<std::size_t>(m_bucket_count * default_load_factor)};
+            std::size_t m_threshold{};
         public:
-            // TODO
+            // default constructor
+            hash_map();
+            // copy constructor
+            hash_map(const hash_map& other_map);
+            // copy assignment
+            hash_map& operator=(const hash_map& other_map);
+            // move constructor
+            hash_map(hash_map&& other_map);
+            // move assignment
+            hash_map& operator=(hash_map&& other_map);
+            // destructor
+            ~hash_map();
+
+            // iterator
+            class iterator{
+
+            };
     };
 }
 
