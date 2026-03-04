509c3061a2b849161c654e916dfac03d22984941
BAOELIETRAN <bngo@oberlin.edu>
2026-03-03 20:47:17 -0500

    done hash map iterator

--- hash_map/src/hash_map.h
+++ hash_map/src/hash_map.h
@@ -38,47 +38,119 @@ namespace anh_phan{
             ~hash_map() = default;
 
             // iterator
+            // although it is hash map iterator, its core is the bucket iterator
+            // hash map iterator is actually bucket iterator
             class iterator{
                 using bucket = std::list<std::pair<int, int>>;
-                using table = std::vector<bucket>;
                 private:
-                    // hash map 
-                    hash_map* m_map_it{};
+                    hash_map* m_map_ptr{};
                     std::size_t m_bucket_index{};
                     bucket::iterator m_bucket_it{};
                     void skip_empty(){
-                        while (m_bucket_index < m_map_it->hash_table.size()){
-                            if (m_map_it->hash_table[m_bucket_index].empty()){
+                        while (m_bucket_index < m_map_ptr->hash_table.size()){
+                            if (m_map_ptr->hash_table[m_bucket_index].empty()){
                                 m_bucket_index ++;
                             }
                             else{
-                                m_bucket_it = m_map_it->hash_table[m_bucket_index].begin();
+                                m_bucket_it = m_map_ptr->hash_table[m_bucket_index].begin();
                                 break;
                             }
                         }
                     }
                 public:
+                    // declare properties of iterator -- C++ expects this --> make them public
+                    using iterator_category = std::forward_iterator_tag;
+                    using difference_type = std::ptrdiff_t;
+                    using value_type = std::pair<int, int>;
+                    using pointer = value_type*;
+                    using reference = value_type&;
+
                     // default constructor
-                    iterator() = default;
+                    iterator() = delete;
                     // direct constructor
-                    explicit iterator(hash_map* map_ptr, std::size_t index) : m_map_it{map_ptr}, m_bucket_index{index}{
-                        if (m_bucket_index < map_ptr->m_capacity){   
-                            m_bucket_it = map_ptr->hash_table[m_bucket_index].begin();
+                    // if same map --> differentiate by m_bucket_index
+                    iterator(hash_map* map_ptr, std::size_t index) : m_map_ptr{map_ptr}, m_bucket_index{index}{
+                        if (m_bucket_index < m_map_ptr->hash_table.size()){
+                            m_bucket_it = m_map_ptr->hash_table[m_bucket_index].begin();
                         }
                         skip_empty();
-                    };
+                    }
                     // copy constructor
-                    iterator(const iterator& other_it) = default;
+                    iterator(const iterator& other_iter) = default;
                     // copy assignment
-                    iterator& operator=(const iterator& other_it) = default;
+                    iterator& operator=(const iterator& other_iter) = default;
+
+                    /* member functions */
+                    // return the address of the current element
+                    pointer operator->() const{
+                        return m_bucket_it.operator->();
+                    }
+
+                    // return the value of the current element
+                    reference operator*() const{
+                        return m_bucket_it.operator*();
+                    }
+        
+                    // prefix increment
+                    // move the m_bucket_it
+                    iterator& operator++(){
+                        if (m_bucket_index == m_map_ptr->hash_table.size()){   
+                            return *this;
+                        }
+                        m_bucket_it ++;
+                        if (m_bucket_it == m_map_ptr->hash_table[m_bucket_index].end()){
+                            m_bucket_index ++;
+                            skip_empty();
+                        }
+                        return *this;
+                    }
+
+                    // postfix increment
+                    // move the m_bucket_it
+                    iterator operator++(int){
+                        auto temp = *this;
+                        (*this).operator++();
+                        return temp;
+                    }
+
+                    // get the key from the iterator
+                    // always ensure that m_bucket_it != bucket.end()
+                    int get_key() const{
+                        return m_bucket_it->first;
+                    }
+
+                    // get the value from the iterator
+                    // always ensure that m_bucket_it != bucket.end()
+                    int get_value() const{ 
+                        return m_bucket_it->second;
+                    }
+
+                    // whether 2 iterators are the same
+                    bool operator==(const iterator& other_it) const{
+                        if (m_map_ptr != other_it.m_map_ptr){
+                            return false;
+                        }
+                        if (m_bucket_index != other_it.m_bucket_index){
+                            return false;
+                        }
+                        // if both are at ends
+                        if (m_bucket_index == m_map_ptr->hash_table.size()){
+                            return true;
+                        }
+                        return m_bucket_it == other_it.m_bucket_it;
+                    }
+
+                    // whether 2 iterators are not the same
+                    bool operator!=(const iterator& other_it) const{
+                        return !((*this).operator==(other_it));
+                    }
             };
             iterator begin(){
-                // this -- pointer to the current object (class hash_map)
-                iterator begin_it{this, 0};
+                iterator begin_it(this, 0);
                 return begin_it;
             }
             iterator end(){
-                iterator end_it{this, m_capacity};
+                iterator end_it(this, hash_table.size());
                 return end_it;
             }
     };
